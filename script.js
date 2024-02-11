9281734d3328e2d4a0062dd8396217a98fae7c7b
Maryam Arshad <156906831+maryamarshad248@users.noreply.github.com>
2024-02-11 17:23:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const arr = [23, 11, 64, 73, 75, 90, 34];
+console.log(arr[0]);
+console.log(arr.at(0));
+console.log(arr.at(5));
+console.log(arr.at(-1));
+
+// getting last array element
+
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('Maryam'.at(0));
+console.log('Maryam'.at(-1));
